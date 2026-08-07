aee4607e2a2a806b56522933730f41f39a8fcd59
Satrajit Sahu <satrajitsahu11@gmail.com>
2026-08-08 00:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (41.79%) - LeetSync

---
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int res=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(n%(i+1)==0){
+                res+=nums[i]*nums[i];
+            }
+        }
+        return res;
+    }
+};
