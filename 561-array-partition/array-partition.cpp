5cebb40e481dfb6f72d05e42af7e5617de77286a
Satrajit Sahu <satrajitsahu11@gmail.com>
2026-08-18 22:43:59 +0530

    Time: 11 ms (54.65%) | Memory: 32 MB (86.88%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -3,7 +3,8 @@ public:
     int arrayPairSum(vector<int>& nums) {
         std::sort(nums.begin(),nums.end());
             int max=0;
-            for(int i=0;i<nums.size();i+=2){
+            int n=nums.size();
+            for(int i=0;i<n;i+=2){
                 max+=nums[i];
             }
         return max; 
