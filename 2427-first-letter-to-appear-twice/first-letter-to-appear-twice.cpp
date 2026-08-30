a35a265fb9d75fc33819d5b039db79bcd1f846e0
Satrajit Sahu <satrajitsahu11@gmail.com>
2026-08-30 19:41:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (5.76%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_set<char>seen;
+        for(char c:s){
+            if(seen.count(c)){
+                return c;
+            }
+            seen.insert(c);
+        }
+        return ' ';
+    }
+};
