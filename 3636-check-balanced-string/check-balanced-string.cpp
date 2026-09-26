7f2ae2ee0754290752fcf162a2bfc0035c978361
Satrajit Sahu <satrajitsahu11@gmail.com>
2026-09-26 20:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (34.02%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int n=num.length();
+        int a=0;
+        int b=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                a+=num[i]-'0';
+            }else{
+                b+=num[i]-'0';
+            }
+        }
+        return a==b;
+    }
+};
